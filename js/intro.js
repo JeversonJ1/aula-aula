0706b521cfbaa8e3d26bfa78ebddbfc0cf9665ff
JeversonJ1 <jeversonbrito@outlook.com>
2025-06-27 11:57:33 -0300

    introduçaõ ao dom

--- js/intro.js
+++ js/intro.js
@@ -1,25 +1,56 @@
-let idade = 25;
-let none = "joaquin";
-let idade2 ="23";
-let resultado1 = idade + Number (idade2);
-console;log(resultado1);
+const formulario = document.getElementById("formulario");
+const resultado = document.getElementById("resultado");
+let idade = 0;// escopo global
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    idade = document.getElementById("idade").value;
+    
+    if(idade < 12){
+    
+    resultado.innerHTML ="voce é criança"
+    let nome = "joao";
+    resultado.innerHTML = nome
+    
+    }else if(idade >=12 && idade < 18){
+        resultado.innerHTML = "voce é adolecente"
+        let nome = "ana";
+        resultado.innerHTML  = nome
+        
+    }else if(idade >=12 && idade < 60){
+     resultado.innerHTML = "voce é adulto"
+     let nome = "carlos";
+     resultado.innerHTML = nome;
+    
+    } else{
+        resultado.innerHTML ="voce é idoso"
+        let nome = "maria";
+        resultado.innerHTML =nome;
+    }
+    
+}
+
+)
+
 //contenaçao texto + numero
 
 /*
-// console.log("ola mundo!");
-// console.info("ola mundo!");
-// console.warn("cuidado,mundo!");
-// console.error("erro.mundo!");
-// console.debug("depuração mundo!");
+ console.log("ola mundo!");
+ console.info("ola mundo!");
+ console.warn("cuidado,mundo!");
+ console.error("erro.mundo!");
+ console.debug("depuração mundo!");
+ palavras chaves para a criação de variaveis no JS 
+ let = variavel que pode ser alteradas
+ const = variavel que não pode ser alteradas
 */
 
 //escopo de variaveis
 //chaves abre bloco de codigo
 //fora do escopo é global
-let idade3 = 30;
-{
+
+
 //dentro do escopo é local    
-let idade3 = 35;
-console.log(idade3);
-}
+
+
+    
 
