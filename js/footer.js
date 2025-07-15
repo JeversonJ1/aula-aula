2aa102b6afd9dd775f25bf076ae799d7659f195c
JeversonJ1 <jeversonbrito@outlook.com>
2025-07-15 11:57:36 -0300

    5555

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+const rodape = document.getElementById("footer");
+rodape.innerHTML = `<nav>
+            <a href="index.html">home</a>
+            <a href="sobre.html">sobre</a>
+            <a href="servicos.html">serviços</a>
+        </nav>`
---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+const nav = document.getElementById("navbar");
+nav.innerHTML = `  <img src="img/logo.svg" alt="logo da pagina">
+        <nav>
+            <a href="index.html" id ="ancora-home">home</a>
+            <a href="sobre.html">sobre</a>
+            <a href="contato.html">contato</a>
+            <a href="servico.html">serviço</a>
+        </nav>
+            <ul>
+                <li>
+                    <a href="img/facebook.svg"alt="link do facebook"class"redes"></a>
+                    <li>
+                        <li>
+                            <a href="#">instagram</a>
+                            <li>
+                                <a href="#">Github</a>
+
+                            </li>
+                        </li>
+                    </li>
+                </li>
+            </ul>
+        </nav>`
